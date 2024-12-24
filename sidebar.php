f01c505f159301503453c16f45a9d4be9bdc9243
Nick Hayward <info@fcswebsites.co.uk>
2024-12-24 17:47:03 +0000

    mobile fixes

--- sidebar.php
+++ sidebar.php
@@ -11,11 +11,11 @@
 <div class="three columns">
 	<?php
 	if ( is_page() && is_active_sidebar( 'page-sidebar' ) ) {
-		echo '<div id="pageSidebar"><ul id="sidebar">';
+		echo '<div id="page-sidebar"><ul id="sidebar">';
 		dynamic_sidebar( 'page-sidebar' );
 		echo '</ul></div>';
 	} elseif ( is_active_sidebar( 'blog-sidebar' ) ) {
-		echo '<div id="blogSidebar"><ul id="sidebar">';
+		echo '<div id="blog-sidebar"><ul id="sidebar">';
 		dynamic_sidebar( 'blog-sidebar' );
 		echo '</ul></div>';
 	}
